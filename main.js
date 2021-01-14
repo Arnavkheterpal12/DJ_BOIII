e7c856ea46518e9653663a91c0339d9b1af34e3e
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2021-01-14 19:47:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song="";
+function preload(){
+    song=loadSound("Imagine Dragons- Believer.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO,600,500);
+    video.hide();
+
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function play(){
+    song.play(song);
+}
